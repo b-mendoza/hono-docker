83c63c9dcb0885d006ada5e24af4b3f65f0de9b4
Bryan Mendoza <bmendoza.dev@hotmail.com>
2024-09-02 11:36:24 -0600

    feat: update `rsbuild` config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from '@rsbuild/core';
+// import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
 
 export default defineConfig({
   output: {
     target: 'node',
   },
+  // environments: {
+  //   web: {
+  //     output: {
+  //       target: 'web',
+  //     },
+  //   },
+  //   node: {
+  //     source: {
+  //       entry: {
+  //         index: './src/index.server.ts',
+  //       },
+  //     },
+  //     output: {
+  //       // Use 'node' target for the Node.js outputs
+  //       target: 'node',
+  //     },
+  //   },
+  // },
   source: {
     entry: {
       index: {
@@ -19,10 +38,18 @@ export default defineConfig({
     rspack: {
       output: {
         chunkFormat: 'module',
+        // library: {
+        //   type: 'module',
+        // },
       },
       experiments: {
         outputModule: true,
+        // topLevelAwait: true,
       },
+      // optimization: {
+      //   usedExports: false,
+      // },
     },
   },
+  // plugins: [pluginNodePolyfill()],
 });
