32289d77a24a1f6e606d2b439bd7023274e750f1
Bryan Mendoza <bmendoza.dev@hotmail.com>
2024-09-02 21:04:34 -0600

    refactor: update `esbuild` config
    Disable bundling and minification to improve build speed and debugging. Enable `sourcemap` generation for better error tracing. Set specific Node.js and ECMAScript targets for compatibility.

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,11 +1,15 @@
 import * as esbuild from 'esbuild';
 
 await esbuild.build({
-  bundle: true,
+  // bundle: false,
+  // packages: 'bundle',
+
   entryPoints: ['./src/index.ts'],
   format: 'esm',
   inject: ['./other/shim.js'],
-  minify: true,
+  minify: false,
   outfile: './dist/index.js',
   platform: 'node',
+  sourcemap: true,
+  target: ['node20.17.0', 'es2022'],
 });
