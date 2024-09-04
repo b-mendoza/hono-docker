9fca30a986719816fa395f30e894f319fd74f813
Bryan Mendoza <bmendoza.dev@hotmail.com>
2024-09-04 13:05:22 -0600

    feat: update `main` entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
+// import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+// import UserAgent from 'user-agents';
+
+import type { HttpBindings } from '@hono/node-server';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { isbot } from 'isbot';
+import * as http from 'node:http';
 import { chromium } from 'playwright';
 import { addExtra } from 'playwright-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import UserAgent from 'user-agents';
 
-const app = new Hono();
+type HonoBindings = HttpBindings & {};
+
+const app = new Hono<{
+  Bindings: HonoBindings;
+}>();
+
+const PORT = Number.parseInt(process.env['PORT'] ?? '3000', 10);
 
 app.get('/', async (c) => {
-  // const { default: UserAgent } = await import('user-agents');
+  const { default: UserAgent } = await import('user-agents');
 
   const userAgent = new UserAgent({
     deviceCategory: 'desktop',
@@ -33,8 +43,8 @@ app.get('/', async (c) => {
 
   const browser = await chromiumWithExtra.launch({
     // headless: process.env['NODE_ENV'] === 'production',
-    headless: false,
 
+    headless: true,
     slowMo: 500,
   });
 
@@ -44,38 +54,40 @@ app.get('/', async (c) => {
       height: 1080,
       width: 1920,
     },
-    proxy: {
-      password: 'mvqzmua70clw',
-      server: 'http://brd.superproxy.io:22225',
-      username: 'brd-customer-hl_fbba4a56-zone-melonook_datacenter',
-    },
+    // proxy: {
+    //   password: 'mvqzmua70clw',
+    //   server: 'http://brd.superproxy.io:22225',
+    //   username: 'brd-customer-hl_fbba4a56-zone-melonook_datacenter',
+    // },
     // javaScriptEnabled: false,
   });
 
   const page = await browserContext.newPage();
 
-  await page.goto('https://www.browserscan.net/');
+  await page.goto('https://playwright.dev/');
 
-  // const titleLocator = page.getByRole('heading', {
-  //   level: 1,
-  // });
+  const titleLocator = page.getByRole('heading', {
+    level: 1,
+  });
 
-  // const titleTextContent = await titleLocator.textContent();
+  const titleTextContent = await titleLocator.textContent();
 
-  // await browserContext.close();
+  await browserContext.close();
 
-  // await browser.close();
+  await browser.close();
 
   return c.json({
-    pageTitle: '',
+    pageTitle: titleTextContent,
   });
 });
 
-const port = Number.parseInt(process.env['PORT'] ?? '3000', 10);
-
-console.log(`Server is running on port http://localhost:${port}`);
-
-serve({
+// @ts-expect-error -- Hono's `serve` function is using a union type for the `serve` function, not a generic
+const server: http.Server = serve({
+  createServer: http.createServer,
   fetch: app.fetch,
-  port,
+  port: PORT,
+});
+
+server.on('listening', () => {
+  console.log(`Server is listening on port http://localhost:${PORT}`);
 });
