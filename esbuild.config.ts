5efab91bda3dafb956fc11b41234e8c407f24e5e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2024-09-04 13:05:05 -0600

    feat: update ESBuild config

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,15 +1,16 @@
 import * as esbuild from 'esbuild';
 
 await esbuild.build({
+  // packages: 'bundle',
+  // sourcemap: true,
+
   bundle: true,
-  entryPoints: ['./src/index.ts'],
+  entryPoints: ['./src/main.ts'],
   format: 'esm',
   inject: ['./other/esm-shim.ts'],
   minify: false,
   outdir: './dist',
-  packages: 'bundle',
   platform: 'node',
-  sourcemap: true,
   splitting: true,
   target: ['node20.17.0', 'es2022'],
 });
